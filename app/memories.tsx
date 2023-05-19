9fa95b27ef138b8c53500b0b669a994af9e5c0aa
Filipe Bacof <filipebacof@gmail.com>
2023-05-19 19:05:48 -0300

    listagem das memorias concluido

--- app/memories.tsx
+++ app/memories.tsx
@@ -5,16 +5,46 @@ import * as SecureStore from 'expo-secure-store'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { Link, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
+import { api } from '../src/lib/api'
+
+dayjs.locale(ptBr)
+
+interface Memory {
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+  id: string
+}
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
   const router = useRouter()
+  const [memories, setMemories] = useState<Memory[]>([])
 
   async function signOut() {
     await SecureStore.deleteItemAsync('token')
 
     router.push('/')
   }
+
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
 
   return (
     <ScrollView
@@ -40,37 +70,38 @@ export default function NewMemory() {
       </View>
 
       <View className="mt-6 space-y-10">
-        <View className="space-y-4">
-          <View className="flex-row items-center gap-2">
-            <View className="h-px w-5 bg-gray-100"></View>
-            <Text className="font-body text-xs text-gray-100">
-              12 de Abril, 2023
-            </Text>
-          </View>
-          <View className="space-y-4 px-8">
-            <Image
-              source={{
-                uri: 'http://192.168.1.3:3333/uploads/389b6458-841a-4cc9-9f0a-eb72f41559cb.jpg',
-              }}
-              className="aspect-video w-full rounded-lg"
-              alt=""
-            />
-            <Text className="font-body text-base leading-relaxed text-gray-100">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-              nam facere odit temporibus ipsum maiores sit numquam voluptatem,
-              commodi repellendus et adipisci minima eum nihil aperiam!
-              Blanditiis veniam repudiandae unde!
-            </Text>
-            <Link href="/memories/id" asChild>
-              <TouchableOpacity className="flex-row items-center gap-2">
-                <Text className="font-body text-sm text-gray-200">
-                  Ler Mais
+        {memories.map((memory) => {
+          return (
+            <View className="space-y-4" key={memory.id}>
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-100"></View>
+                <Text className="font-body text-xs text-gray-100">
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
                 </Text>
-                <Icon name="arrow-right" size={16} color="#9e9ea0" />
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
+              </View>
+              <View className="space-y-4 px-8">
+                <Image
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                  className="aspect-video w-full rounded-lg"
+                  alt=""
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="font-body text-sm text-gray-200">
+                      Ler Mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
   )
