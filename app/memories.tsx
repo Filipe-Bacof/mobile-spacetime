8eb74eb6f06e5c2f201efa5ef7e2596e2e9086fa
Filipe Bacof <filipebacof@gmail.com>
2023-05-19 18:48:33 -0300

    design listagem de memorias

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, View, ScrollView } from 'react-native'
+import { TouchableOpacity, View, ScrollView, Text, Image } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 import * as SecureStore from 'expo-secure-store'
 
@@ -18,10 +18,10 @@ export default function NewMemory() {
 
   return (
     <ScrollView
-      className="flex-1 px-8"
+      className="flex-1"
       contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
-      <View className="mt-4 flex-row items-center justify-between">
+      <View className="mt-4 flex-row items-center justify-between px-6">
         <NLWLogo />
 
         <View className="flex-row gap-2">
@@ -36,6 +36,40 @@ export default function NewMemory() {
               <Icon name="plus" size={16} color="#000" />
             </TouchableOpacity>
           </Link>
+        </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-100"></View>
+            <Text className="font-body text-xs text-gray-100">
+              12 de Abril, 2023
+            </Text>
+          </View>
+          <View className="space-y-4 px-8">
+            <Image
+              source={{
+                uri: 'http://192.168.1.3:3333/uploads/389b6458-841a-4cc9-9f0a-eb72f41559cb.jpg',
+              }}
+              className="aspect-video w-full rounded-lg"
+              alt=""
+            />
+            <Text className="font-body text-base leading-relaxed text-gray-100">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              nam facere odit temporibus ipsum maiores sit numquam voluptatem,
+              commodi repellendus et adipisci minima eum nihil aperiam!
+              Blanditiis veniam repudiandae unde!
+            </Text>
+            <Link href="/memories/id" asChild>
+              <TouchableOpacity className="flex-row items-center gap-2">
+                <Text className="font-body text-sm text-gray-200">
+                  Ler Mais
+                </Text>
+                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
         </View>
       </View>
     </ScrollView>
